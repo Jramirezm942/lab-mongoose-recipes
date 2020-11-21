b17cdf2d6959718264631b1ef6b49817f5e16d4e
Juan Manuel <jramirezm942@gmail.com>
2020-11-20 22:21:48 -0600

    avance

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
+
+
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { schema } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
